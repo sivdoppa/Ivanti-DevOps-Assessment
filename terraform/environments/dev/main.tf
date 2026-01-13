d8de0a18d4f655646f5f4dbf4c5ac62051e0c761
Sivaram Krishna <sivaramk185@gmail.com>
2026-01-13 12:24:15 +0000

    Updated main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -15,14 +15,6 @@ terraform {
 
 provider "azurerm" {
   features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
-  resource_provider_registrations = "none"
 }
 
 locals {
