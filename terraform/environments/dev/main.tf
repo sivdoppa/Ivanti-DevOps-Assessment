31951ec239dddfe97a812045cb95c679141a4afa
Sivaram Krishna <sivaramk185@gmail.com>
2026-01-13 06:07:19 +0000

    Updated main.tf by removing k8 version from it

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -92,7 +92,6 @@ module "aks" {
   location                   = local.location
   resource_group_name        = azurerm_resource_group.main.name
   dns_prefix                 = "aks-${local.environment}"
-  kubernetes_version         = var.kubernetes_version
   environment                = local.environment
   
   # System node pool
