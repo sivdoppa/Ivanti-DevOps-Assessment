c38ce787cd4675f5efd67875618bd299f699226c
Sivaram Krishna <sivaramk185@gmail.com>
2026-01-13 12:19:41 +0000

    Updated backend.tf

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatedevops1768147514"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
