731de60c9b0438e95f4956019c1f782984013ea5
Sivaram Doppalapudi <sivaramdoppalapudi@Sivarams-MacBook.local>
2026-01-14 09:28:40 +0530

    Edited the pipeline stage, added outputs file, refactored the code pipeline

---
+++ terraform/environments/prod/outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "aks_cluster_name" {
+  value = module.aks.cluster_name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.main.login_server
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.main.name
+}
