3a13747b06f8cb39e5a06a873291ff15b5a38a33
Sivaram Doppalapudi <sivaramdoppalapudi@Sivarams-MacBook.local>
2026-01-14 09:47:22 +0530

    fixing oidc issue

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -35,6 +35,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = true
+
   network_profile {
     network_plugin     = "azure"
     network_policy     = "calico"
